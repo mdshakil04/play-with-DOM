98a944dcbee9f4909476e8153ed60da91992ed83
mdshakil04 <m.shakil04@yahoo.com>
2025-05-03 07:50:28 +0600

    get Element by ID

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("I am from dom")
+const title = document.getElementById("title")
+title.innerText = "Shakil is my Boss";
+title.style.color = "blue"
+console.log(title.innerText)
+const liContainer = document.getElementsByClassName("li-container");
+// console.log(liContainer)
