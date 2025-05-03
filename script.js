28b63834114d2403f822768627c75ea8aa88cee5
mdshakil04 <m.shakil04@yahoo.com>
2025-05-03 19:58:29 +0600

    For of

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const title = document.getElementById("title")
 title.innerText = "Shakil is my Boss";
 title.style.color = "blue"
 console.log(title.innerText)
-const liContainer = document.getElementsByClassName("li-container");
-// console.log(liContainer)
+const liContainer = document.getElementsByClassName("li-items");
+const liItems = document.getElementsByClassName("li-items")
+// console.log(liItems)
+for(const li of liItems){
+    console.log(li)
+}
