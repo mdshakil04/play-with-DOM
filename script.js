bed8f06c97382fe9d829469459d5fbd09a2a6324
mdshakil04 <m.shakil04@yahoo.com>
2025-05-03 21:15:33 +0600

    Problem solved

--- script.js
+++ script.js
@@ -7,5 +7,11 @@ const liContainer = document.getElementsByClassName("li-items");
 const liItems = document.getElementsByClassName("li-items")
 // console.log(liItems)
 for(const li of liItems){
-    console.log(li)
+    // console.log(li)
+    li.innerText = "Boss"
+}
+const pTag = document.getElementsByTagName('p')
+for (const p of pTag){
+    // console.log(p.innerText)
+    p.innerText = "How are you ?"
 }
