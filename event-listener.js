6b735911191321a2da26116d08702053b170906d
mdshakil04 <m.shakil04@yahoo.com>
2025-05-07 22:14:21 +0600

    another option done

--- event-listener.js
+++ event-listener.js
@@ -1,11 +1,19 @@
 // console.log('Event listener')
-const bgBtn = document.getElementById('click-maro');
+// const bgBtn = document.getElementById('click-maro');
 // console.log(bgBtn)
 // Option 1
 // bgBtn.onclick = function bgChange(){
 //     document.body.style.backgroundColor = 'red';
 // }
 // Option 2
-bgBtn.addEventListener('click', function makeGreen(){
+// bgBtn.addEventListener('click', function makeGreen(){
+//     document.body.style.backgroundColor = 'green';
+// })
+// Option 3
+document.getElementById('click-maro').addEventListener('click', function(){
     document.body.style.backgroundColor = 'blue';
 })
+// Other option 
+function colorChange(){
+    document.body.style.backgroundColor = 'red';
+}
