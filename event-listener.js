698e3cd2245f4513f20232ccf1201c182b78bf65
mdshakil04 <m.shakil04@yahoo.com>
2025-05-07 22:05:00 +0600

    event listener added to the btn

--- event-listener.js
+++ event-listener.js
@@ -1,6 +1,11 @@
 // console.log('Event listener')
 const bgBtn = document.getElementById('click-maro');
 // console.log(bgBtn)
-bgBtn.onclick = function bgChange(){
-    document.body.style.backgroundColor = 'red';
-}
+// Option 1
+// bgBtn.onclick = function bgChange(){
+//     document.body.style.backgroundColor = 'red';
+// }
+// Option 2
+bgBtn.addEventListener('click', function makeGreen(){
+    document.body.style.backgroundColor = 'blue';
+})
