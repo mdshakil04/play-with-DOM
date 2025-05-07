498912314bd0eb52fe32f29e8c30a9260e5b0772
mdshakil04 <m.shakil04@yahoo.com>
2025-05-07 21:33:18 +0600

    Btn-event done

--- event-listener.js
+++ event-listener.js
@@ -1,1 +1,6 @@
-console.log('Event listener')
+// console.log('Event listener')
+const bgBtn = document.getElementById('click-maro');
+// console.log(bgBtn)
+bgBtn.onclick = function bgChange(){
+    document.body.style.backgroundColor = 'red';
+}
