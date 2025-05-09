7722f8227780404959bbf4f1f151e31f7e63a29a
mdshakil04 <m.shakil04@yahoo.com>
2025-05-09 13:14:42 +0600

    Some code iupdated

--- event.js
+++ event.js
@@ -1,10 +1,21 @@
 // console.log('from event.html file')
-document.getElementById('update-btn').addEventListener('click', function(){
-    // console.log('btn clicked')
-    const inputField = document.getElementById('input-field')
-    const inputText = inputField.value;
-    // console.log(inputText)
-    const textContainer = document.getElementById('default-text');
-    textContainer.innerText = inputText;
-    inputField.value = '';
-})
+document.getElementById("update-btn").addEventListener("click", function () {
+  // console.log('btn clicked')
+  const inputField = document.getElementById("input-field");
+  const inputText = inputField.value;
+  // console.log(inputText)
+  const textContainer = document.getElementById("default-text");
+  textContainer.innerText = inputText;
+  inputField.value = "";
+});
+document.getElementById("comment-btn").addEventListener("click", function () {
+  // console.log('btn clicked')
+  const commentsContainer = document.getElementById("comments-container");
+  const commentsBox = document.getElementById("comment-box");
+  const comments = commentsBox.value;
+  // console.log(comments)
+  const commentsText = document.createElement("li");
+  commentsText.innerText = comments;
+  commentsContainer.appendChild(commentsText);
+  commentsBox.value = "";
+});
